b6f2365c4c4bdfa237013e2c9412fab3974647a5
Rawan <rawanmohammed99@gmail.com>
2021-12-15 15:55:43 +0200

    Update codePart1.asm

--- codePart1.asm
+++ codePart1.asm
@@ -10,7 +10,12 @@ array: db 1,20,5,99,6,12,44,32,5,6
 .CODE               
 mov si,00   
 mov cx,10   ;The number of array elements                           
-sub cx,1    ;The number of array element minus1 
+sub cx,1    ;The number of array element minus1  
+BubbleSortArray:
+cmp cx,si
+jz Next
+mov al, array1[si] 
+mov bl, array1[si+1]
 
 
 
