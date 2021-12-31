038c546aec3b0ba8aca59fe8f474b5c215797038
SalahKhalil1 <elmashtoly443@gmail.com>
2022-01-01 00:04:24 +0200

    Update selectionsort.asm
    outer loop of selection sort

--- selectionsort.asm
+++ selectionsort.asm
@@ -1,40 +1,13 @@
-.386
-.model small
-.data
-maxIndex		Dword 	? ; variable holds the index of the maximum element
-L1_Counter		Dword 	? ; holds counter of the out loop
-L2_Counter		Dword 	? ; holds counter of the inside loop
-Array_Size			dword	? ; Array Size
-Array1			Dword	100000 dup (?) ; Creating array
-.code
-Selection_Algorithm:
-	lea ESI,  Array1 
-		mov EAX, Array_Size
-		mov L1_Counter,EAX
-	Loop_1:
-		cmp L1_Counter,0
-		jle Out_1
-		dec L1_Counter
-		mov EAX,L1_Counter
-		mov maxIndex,EAX
-		dec EAX
-		mov L2_Counter,EAX
-		lea EAX, Array1
-
-	Loop_2: 
-		cmp InnerCounter,0
-		jl exit1
-		lea ESI,  array	
-		MOV EDI , maxIndex				; EDI = Counter index
-		MOV EDX , L2_Counter				;edx=Loop 2 counter        
-		SHL EDI,2					; EDI = indexMax * 4 
-		SHL EDX,2					; ECX = (Loop 2 counter ) * 4
-		ADD EDI,ESI					; EDI =  Array offset + (Max index ) * 4 , EDI = &arr [Maxindex]
-		ADD EDX,ESI					; EDX =  Array offset + (Loop 2 counter) * 4, EDX= &arr [Loop 2 counter]
-		MOV EAX,[EDI]					; EAX = arr [Max index]
-		mov EBX,[EDX]					; EBX = arr [Loop 2 counter] 
-		CMP EBX,EAX					;arr[Loop 2 counter] > arr[max index] 
-		jle out					;jump if a[max index]>arr[Loop 2 counter]
-		mov edx,L1_Counter				; if arr[Loop 2 counter] > arr[Max index]
-		mov maxIndex,edx				;max index =Loop2counter
-
+; Selection Sort begin
+Selection_Sort: 
+		mov ESI,  offset Array ;loading array base address in esi
+		mov ecx,arr_size         ;ecx = arr size
+		
+Select_loop1:                    ; label
+		cmp ecx,0                ;compare ecx with 0
+		jle exit_loop_1          ; exit loop label
+		dec ecx                  ; decrement the counter by 1          
+		mov MaxIndex,ecx         ; max index = current index
+		push ecx                 ; push outer loop counter to save it from changes
+		dec ecx	                 ; decrement counter to be used in inner loop 
+		lea ESI,  Array          ;loading array base address in esi
