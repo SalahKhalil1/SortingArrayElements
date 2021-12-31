0f0b9e7537d97fed26a9f3aa32353f8dc044714c
moaaz1010 <47803207+moaaz1010@users.noreply.github.com>
2021-12-31 18:23:35 +0200

    Update codePart1.asm

--- codePart1.asm
+++ codePart1.asm
@@ -1,14 +1,12 @@
 
-include 'emu8086.inc'
-
-org 100h
-.model small
-
-.data
-
-.code
-
-main proc
+;------------------------
+Bubble_Sort:
+		mov ESI, offset Array					; Load ESI with start address of the Array
+		MOV eax, arr_size					; get the lenght of array in eax
+		CMP arr_size, 1
+		JE Output				; if there is only one element in the array skip the bubble sort algorithm
+		dec eax						; dec eax by 1 
+		mov ecx,0			; outloop = arr_size-1
 
 L2:
 ;shift the  inner Loop index
