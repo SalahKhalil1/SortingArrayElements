e9bf3db086fd016fc05858d539182eab582c172f
moaaz1010 <47803207+moaaz1010@users.noreply.github.com>
2021-12-31 23:10:40 +0200

    Update codePart1.asm

--- codePart1.asm
+++ codePart1.asm
@@ -7,6 +7,12 @@ Bubble_Sort:
 		JE Output				; if there is only one element in the array skip the bubble sort algorithm
 		dec eax						; dec eax by 1 
 		mov ecx,0			; outloop = arr_size-1
+; Start Outer Loop
+L1:
+		cmp ecx,eax
+		jge Output
+		push ecx
+		mov ecx ,0					; push ECX to the stack to keep the outLoop counter from chainging
 
 L2:
 ;shift the  inner Loop index
@@ -15,7 +21,14 @@ mov ebx,edx ;ebx=i
 push edx ;edx will be pushed
 mov edx,eax ;edx = size-1
 neg ebx
-add edx,ebx ;edx=size-1-i 
+add edx,ebx ;edx=size-1-i
+
+
+
+exit_L2:
+	pop ecx
+	inc ecx
+	jmp L1 
   
 
           
